818fcf54cb695cafb00878b133018194b47c78fe
Amber Collins <collinsa1@ufl.edu>
2026-06-15 14:52:02 -0400

    Added ToggleBackbone script and tested ToggleOrbitals and ToggleBackbone scripts
    We already had a toggle orbital script, I just made sure it worked since we weren't currently using it. I made the toggle backbone script, and it is fully functional too. Scripts are in a new folder in Scripts > Radial Menu Options.

---
+++ Assets/Scripts/Radial Menu Options/ToggleOrbitals.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class ToggleOrbitals : MonoBehaviour
+{
+    [SerializeField] private XRRayInteractor rayInteractor;
+    public InputActionProperty primaryButton;
+
+    private Dictionary<GameObject, bool> orbitalStates = new Dictionary<GameObject, bool>();
+
+    private void OnEnable()
+    {
+        primaryButton.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        primaryButton.action.Disable();
+    }
+
+    private void Update()
+    {
+        if (!primaryButton.action.WasPressedThisFrame()) return;
+        if (rayInteractor == null) return;
+
+        rayInteractor.TryGetCurrentRaycast(
+            out RaycastHit? raycastHit,
+            out _,
+            out _,
+            out _,
+            out bool isUIHitClosest
+        );
+
+        if (isUIHitClosest || !raycastHit.HasValue) return;
+
+        // Walk up hierarchy to find molecule root (mirrors RayPointerLogger logic)
+        GameObject hit = raycastHit.Value.collider?.gameObject;
+        if (hit == null) return;
+
+        GameObject molecule = hit;
+        if (hit.transform.parent != null)
+        {
+            molecule = hit.transform.parent.gameObject;
+            if (molecule.transform.parent != null)
+                molecule = molecule.transform.parent.gameObject;
+        }
+
+        if (molecule.tag != "Molecule") return;
+        if (molecule.transform.childCount == 0) return;
+
+        // Toggle orbital (child 0 = .cub orbital surface)
+        if (!orbitalStates.ContainsKey(molecule))
+            orbitalStates[molecule] = true;
+
+        orbitalStates[molecule] = !orbitalStates[molecule];
+        molecule.transform.GetChild(0).gameObject.SetActive(orbitalStates[molecule]);
+    }
+}
